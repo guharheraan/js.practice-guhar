89236db699b0278b7377176ea0318fbf0c6b52b6
guharheraan <guhar.heraan@gmail.com>
2024-03-16 15:30:26 +0500

    operations and comparison operators

---
+++ comparisons.js
@@ -0,0 +1,31 @@
+// comparison operators
+
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+
+// the result will be either true or false
+
+// but here is a problem in it when compare the Null with 0 then
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+// console.log(null != 0);
+
+// this is bcz the coparison  operator try to convert the null and undefined value into number using ToNumber conversion method
+
+//strict check operator
+
+// the  strict equality operator (===) returns true if the values  of two operands are strictly equal, and also
+// '===' means that both value and type should match
+// '!==' means that both value and type should not match
+
+console.log(3 === '3'); //false because one of them is string other is number so they are different types hence false
+
+console.log('Hello' === 'Hello'); //true because both values and types same
---
+++ operations.js
@@ -0,0 +1,36 @@
+// operations
+
+console.log(3 + 5);
+console.log(3 - 5);
+console.log(3 * 5);
+console.log(3 / 5);
+console.log(3 ** 5); // power  operator
+console.log(3 % 5); // modulus operator
+
+let st1 = 'hello';
+let st2 = 'world';
+let st3 = st1 + st2;
+
+console.log(st3);
+
+// prefix and postfix operator placement
+
+// let x = 4;
+// let y = x++;
+
+// console.log('x: ${x}, y: ${y}');
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+// let a = 3;
+// const b = ++a;
+
+// console.log('a: ${a} , b: ${b}');
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
