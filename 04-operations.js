d07d418f99090bb0fe5a0c735ee3426e5e5ffe27
guharheraan <guhar.heraan@gmail.com>
2024-03-16 16:16:29 +0500

    updated

--- 04-operations.js
+++ 04-operations.js
@@ -15,22 +15,22 @@ console.log(st3);
 
 // prefix and postfix operator placement
 
-// let x = 4;
-// let y = x++;
+let x = 4;
+let y = x++;
 
-// console.log('x: ${x}, y: ${y}');
+console.log(`x: ${x}, y: ${y}`);
 
-let x = 3;
-const y = x++;
+// let x = 3;
+// const y = x++;
 
-console.log(`x:${x}, y:${y}`);
-
-// let a = 3;
-// const b = ++a;
-
-// console.log('a: ${a} , b: ${b}');
+// console.log(`x:${x}, y:${y}`);
 
 let a = 3;
 const b = ++a;
 
-console.log(`a:${a}, b:${b}`);
+console.log(`a: ${a} , b: ${b}`);
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
