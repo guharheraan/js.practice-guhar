901d269a52fc849632ce28b5cfd80414175d2448
guharheraan <guhar.heraan@gmail.com>
2024-04-02 14:17:09 +0500

    switch statements in JS

---
+++ 22-switch.js
@@ -0,0 +1,54 @@
+//switch statements in js
+
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// } //this is the basic syntax of the switch statement.
+
+// let's try our own example
+
+// let month = 12;
+
+// switch (month) {
+//   case 1:
+//     console.log('jan');
+//     break; //if we dont use this break then bydefault  it will execute all the code below that particular case
+//   // excepet the default statement.
+//   case 2:
+//     console.log('feb');
+//     break;
+//   case 3:
+//     console.log('mar');
+//     break;
+//   case 4:
+//     console.log('apr');
+//     break;
+//   case 5:
+//     console.log('may');
+//     break;
+//   case 6:
+//     console.log('jun');
+//     break;
+
+//   default:
+//     console.log('default value');
+//     break;
+// }
+
+// const month = 'december';
+
+// switch (month) {
+//   case 'january':
+//     console.log('jan');
+//     break;
+//   case 'december':
+//     console.log('dez');
+//     break;
+//   default:
+//     console.log(`${month}`);
+//     break;
+// }
