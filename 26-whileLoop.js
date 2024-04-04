d96e5eedfa405458b896abd62b9fb090eede7780
guharheraan <guhar.heraan@gmail.com>
2024-04-04 16:52:36 +0500

    DoWhile and While loops in Js

---
+++ 26-whileLoop.js
@@ -0,0 +1,26 @@
+// while and do while loops in Js
+// let i = 0;
+// while (i <= 10) {
+//   console.log(`value is : ${i}`);
+//   i++;
+// }
+
+// let myArr = ['guhar', 'herran', 'muzamil', 'someone'];
+
+// let i = 0;
+// while (i < myArr.length) {
+//   console.log(`the value of array is : ${myArr[i]}`);
+//   i = i + 1;
+// }
+
+// ------------------- do while ----------------------------
+
+let score = 11;
+
+do {
+  console.log(`score is ${score}`);
+  score = score + 1; //we can write it as "score ++" also
+} while (score <= 10);
+
+// if we try the score value more than 10 then the loop will only run for one time and then it will
+// get terminated.
